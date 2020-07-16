57e418fa419080eb3b9351e6d3a77f9da3b36fd6
jairo-pineda <68175163+jairo-pineda@users.noreply.github.com>
2020-07-15 22:37:37 -0600

    agregado los atributos y metodos a usar en la superclase y la clase main a espera de que mis compañeros me den usuarios para poder crear las ramas-

--- GRUPO1/src/PAQUETE/Persona.java
+++ GRUPO1/src/PAQUETE/Persona.java
@@ -9,6 +9,41 @@ package PAQUETE;
  *
  * @author JAIRO PINEDA
  */
-public class Persona {
+public abstract class Persona {
+    
+    //ATRIBUROS
+    private String nombre;
+    private String apellido;
+    private String edad;
+    
+    //METODOS    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido=apellido;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }    
+    public void setEdad(String edad){
+        this.edad=edad;
+    }
+    public String getEdad(){
+        return this.edad;
+    }  
+    public void imprimirNombre(){
+        System.out.println(nombre);
+    }
+    public void imprimirApellido(){
+        System.out.println(apellido);
+    }
+    public void imprimirEdad(){
+        System.out.println(edad);
+    }
+    abstract String setInformacionProfecion();
     
 }
--- GRUPO1/src/grupo1/GRUPO1.java
+++ GRUPO1/src/grupo1/GRUPO1.java
@@ -5,6 +5,9 @@
  */
 package grupo1;
 
+import PAQUETE.Deportista;
+import PAQUETE.Doctor;
+
 /**
  *
  * @author JAIRO PINEDA
@@ -15,7 +18,27 @@ public class GRUPO1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //INSTANCIA DE CLASES
+         Doctor doctor = new Doctor();
+         Deportista deportista = new Deportista();
+         
+         //INSTRUCCION DE IMPRESION DOCTOR
+          System.out.println(doctor.setInformacionProfecion();
+          doctor.imprimirNombre();
+          doctor.imprimirApellido();
+          doctor.imprimirEdad();
+          System.out.println("");
+          
+          //INSTRUCCION DE IMPRESION DEPORTISTA
+          System.out.println(deportista.setInformacionProfecion();
+          deportista.imprimirNombre();
+          deportista.imprimirApellido();
+          deportista.imprimirEdad();
+          System.out.println("");
+         
+         
+         
     }
     
 }
